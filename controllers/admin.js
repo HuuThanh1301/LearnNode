9e256c20335fb9ebe2e0789b1299ef6d1d817e2f
nhthanhdn95 <nhthanhdn95@gmail.com>
2024-02-22 08:22:49 +1030

    Add section 18- Understand Validation

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,13 @@
 const Product = require("../models/product");
+const { validationResult } = require("express-validator");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
-    isAuthenticated: req.session.isLoggedIn,
+    hasError: false,
+    errorMessage: null,
   });
 };
 
@@ -14,6 +16,25 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description,
+      },
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
   const product = new Product({
     title: title,
     price: price,
@@ -50,6 +71,8 @@ exports.getEditProduct = (req, res, next) => {
         editing: editMode,
         product: product,
         isAuthenticated: req.session.isLoggedIn,
+        hasError: false,
+        errorMessage: null,
       });
     })
     .catch((err) => console.log(err));
@@ -61,6 +84,24 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array())
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+      },
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
   Product.findById(prodId)
     .then((product) => {
@@ -79,7 +120,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find({userId: req.user._id})
+  Product.find({ userId: req.user._id })
     .then((products) => {
       res.render("admin/products", {
         prods: products,
--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,68 @@
-const express = require('express');
+const express = require("express");
 
-const authController = require('../controllers/auth');
+const { check, body } = require("express-validator");
+
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+const User = require("../models/user");
 
-router.get('/signup', authController.getSignup);
+router.get("/login", authController.getLogin);
 
-router.post('/login', authController.postLogin);
+router.get("/signup", authController.getSignup);
 
-router.post('/signup', authController.postSignup);
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email."),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    ).isLength({ min: 5 }).isAlphanumeric(),
+  ],
+  authController.postLogin
+);
 
-router.post('/logout', authController.postLogout);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "Email exists already, please pick a different one."
+            );
+          }
+        });
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 
-router.get('/reset', authController.getReset);
+router.post("/logout", authController.postLogout);
 
-router.post('/reset', authController.postReset);
+router.get("/reset", authController.getReset);
 
-router.get('/reset/:token', authController.getNewPassword);
+router.post("/reset", authController.postReset);
 
-router.post('/new-password', authController.postNewPassword);
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
